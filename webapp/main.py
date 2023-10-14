08c3766744674aa744dfc021f036fdffacf433ce
Anh Nhữ Quang <anhnqqe180005@fpt.edu.vn>
2023-10-14 20:14:51 +0700

    Demo page without parameters

--- webapp/main.py
+++ webapp/main.py
@@ -1,1 +1,10 @@
 from nicegui import ui
+
+def main():
+    ui.label('CineScope - Your movies in seconds')
+    ui.input(label='Text', placeholder='Search for a movie')
+    ui.button('Search')
+    
+    ui.run()
+
+main()
