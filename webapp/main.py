48eb90ea05fbb2eecf3c2a9507e429b126e6ec58
Anh Nhữ Quang <anhnqqe180005@fpt.edu.vn>
2023-10-14 22:30:06 +0700

    Little change to main frontend

--- webapp/main.py
+++ webapp/main.py
@@ -2,7 +2,7 @@ from nicegui import ui
 
 def main():
     ui.label('CineScope - Your movies in seconds')
-    ui.input(label='Text', placeholder='Search for a movie')
+    ui.input(placeholder='Search for a movie')
     ui.button('Search')
     
     ui.run()
--- webapp/results.py
+++ webapp/results.py
@@ -1,2 +1,6 @@
 from nicegui import ui
 
+label = ui.label('Hello from NiceGUI!')
+label.set_text('This text is passed in set_text')
+
+ui.run()
